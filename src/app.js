a36afaf2f15babbbcab98fa2a4e338fbd084cd0c
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-06-18 20:27:26 +0000

    Failure

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let textExcuse = document.getElementById("excuse");
+  innerHtml = resultado;
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generateExcuse() {
+    let str = "";
+    function randomNumber(arreglo) {
+      return arreglo[Math.floor(Math.random() * arreglo.length)];
+    }
+    str += randomNumber(who) + " ";
+    str += randomNumber(action) + " ";
+    str += randomNumber(what) + " ";
+    str += randomNumber(when) + " ";
+    return str;
+  }
+
+  let resultado = generateExcuse();
+  console.log(resultado);
 };
